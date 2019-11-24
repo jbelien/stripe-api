3173d3b432793f8dc4c9190a8512ee6a568a0a72
jbelien <dev@jbelien.be>
2019-11-24 21:03:13 +0000

    Apply fixes from StyleCI

--- config/autoload/zfr-cors.global.php
+++ config/autoload/zfr-cors.global.php
@@ -2,18 +2,19 @@
 
 /**
  * This is the config file for ZfrCors. Just drop this file into your config/autoload folder (don't
- * forget to remove the .dist extension from the file), and configure it as you want
+ * forget to remove the .dist extension from the file), and configure it as you want.
+ *
  * @see https://www.apigility.org/documentation/recipes/allowing-request-from-other-domains
  */
 
 return [
     'zfr_cors' => [
-        /**
+        /*
          * Set the list of HTTP verbs.
          */
         'allowed_methods' => ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 
-        /**
+        /*
          * Standard CORS requests do not send or set any cookies by default. For this to work,
          * the client must set the XMLHttpRequest's "withCredentials" property to "true". For
          * this to work, you must set this option to true so that the server can serve
